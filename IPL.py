78403e89b272d893eed367e48d978f1220b2a0d7
Shubham <46809421+shubnimkar@users.noreply.github.com>
2023-04-27 10:19:50 +0530

    Add files via upload

---
+++ IPL.py
@@ -0,0 +1,82 @@
+# # Teams(
+# #     CV: league-Name
+# #     attributes--> 
+# #     IV:name,owner,captain,sponsers,support,staff[role:name]
+# #     Behaviors--> getters/setter/add
+# # )
+# # players(
+# #     attributes--> 
+# #     IV:name,baseprice,actual price,role,nationality,dob,misc-stats
+# #     Behaviors--> getters/setter/add
+# # )
+# # venues(
+# #     attributes-->
+# #     IV:name,location(city:,state:,country),capacity
+# #     Behaviors--> getters/setter/add
+# # )
+# # fixture(
+# # attributes-->
+# # IV: team1,team2,venue,time,winner team
+# # Behaviors--> getters/setter/add/outcome
+# # )
+# #########################################################################################################################################################
+#import random
+# print("WELCOME TO IPL 2023")
+
+
+# class Teams():
+#     def __init__(self): 
+#         name=
+#         owner=
+#         captain=
+#         sponsers=
+#         support_staff=[]#[{role,name}]
+    
+#     def display_league():
+
+
+
+
+
+
+
+
+# class  players():
+#    name=
+#    base_price=
+#    actual_price=
+#    role=
+#    nationality=
+#    DOB=
+#    misc_stats=
+
+
+#class venues():
+    # def __init__(self):
+    #     venue={"Ahmedabad":["Ahmedabad","Narendra Modi Stadium",132,000],
+    #        "Bengaluru":["Bengaluru","M. Chinnaswamy Stadium",40,000],
+    #         "Mumbai":["Mumbai","Wankhede Stadium",33,108],
+    #        "Hyderabad":["Hyderabad","Rajiv Gandhi International Cricket Stadium",55,000],
+    #        "Jaipur":["Jaipur","Sawai Mansingh Stadium",30,000],
+    #        "Delhi":["Delhi","Arun Jaitley Stadium",41,842],
+    #        "Mohali":["Mohali","Punjab Cricket Association Stadium",27,000],
+    #        "Chennai":["Chennai","M. A. Chidambaram Stadium",50,000],
+    #        "Navi Mumbai":["Navi Mumbai","D Y Patil Sports Stadium",55,000],
+    #        "Chennai":["Chennai","M. A. Chidambaram Stadium",50,000]
+    #                }
+
+    # list=['Ahmedabad','Bengaluru','Mumbai','Hyderabad','Jaipur','Delhi','Mohali','Chennai','Navi Mumbai']
+    # print(venue[input(random.choice(list))])
+
+# class fixtures():
+#     match no=
+#     team1=
+#     team2=
+#     venue=
+#     time=
+#     winner_team=
+
+# def main()
+# main()
+
+
